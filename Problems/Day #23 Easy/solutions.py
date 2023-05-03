f6596ec24e3d9248833e5429beb766a855cf578a
bitzs-alex <bitzsalex@gmail.com>
2023-05-03 21:43:44 +0900

    doc: gitignore file was added

--- Problems/Day #23 Easy/solutions.py
+++ Problems/Day #23 Easy/solutions.py
@@ -1,80 +1,146 @@
-from typing import List, Tuple
+from typing import List, Tuple, Set
 
 
-class PathFinder:
-    def __init__(self, board:List[List[bool]]):
-        self.board = board
+def find_minimum_path(
+    board:List[List[bool]], start:Tuple[int,int], end:Tuple[int,int]) -> int:
+    """Minimum path finder
 
-    def find_minimum_path(self, start:Tuple, end:Tuple) -> int:
-        if len(start) != 2 or len(end) != 2: return None
-        return self.find_path(start, end, set())
+    Args:
+        board (List[List[int]]): A list of list that contain booleans
+        start (Tuple[int,int]): A starting position in (row, col)
+        end (Tuple[int,int]): An ending position in (row, col)
 
-    def is_vertical_empty(self, current:Tuple, visited:set, place:str="top") -> bool:
-        x = current[0] - 1 if place == "top" else current[0] + 1
-        coordinate = (x, current[1])
-        return not (
-            x == -1 or x >= len(self.board) or self.board[x][current[1]] or coordinate in visited
-            )
+    Returns:
+        int: The minimum path from start position to end
+    """
+    return recursive_finder(board, start, end, set())
 
-    def is_horizontal_empty(self, current:Tuple, visited:set, place:str="left") -> bool:
-        y = current[1] - 1 if place == "left" else current[1] + 1
-        coordinate = (current[0], y)
-        return not (
-            y == -1 or y >= len(self.board[0]) or self.board[current[0]][y] or coordinate in visited
-            )
-        
-    def add_path_length(self, path_lengths:List, path_length:int) -> None:
-        if path_length != 0:
-            path_lengths.append(path_length)
-            
-        return path_lengths
-     
-    def find_path(self, start:Tuple, end:Tuple, visited:set) -> int:
-        if start == end: return len(visited)
-        
-        path_lengths = []
-        print(start, visited)
-        if self.is_horizontal_empty(start, visited):
-            path_lengths.append(self.find_path((start[0], start[1] - 1), end, visited.union({start})))
-        if self.is_vertical_empty(start, visited):
-            path_lengths.append(self.find_path((start[0] - 1, start[1]), end, visited.union({start})))
-        if self.is_horizontal_empty(start, visited, "right"):
-            path_lengths.append(self.find_path((start[0], start[1] + 1), end, visited.union({start})))
-        if self.is_vertical_empty(start, visited, "bottom"):
-            path_lengths.append(self.find_path((start[0] + 1, start[1]), end, visited.union({start})))
-        
-        return min(path_lengths) + 1 if len(path_lengths) else 0
+
+def recursive_finder(
+    board:List[List[bool]], start:Tuple[int,int], end:Tuple[int,int], visited:Set) -> int:
+    """A recursive function to find the minimum path
+
+    Args:
+        board (List[List[int]]): A board
+        start (Tuple[int,int]): Starting position
+        end (Tuple[int,int]): Ending position
+        visited (Set): A set of visited paths so far
+
+    Returns:
+        int: The minimum path length from starting to ending position
+    """
+    if start == end:
+        # return the number of visited paths, if start and end are equal
+        return len(visited)
+    
+    path_lengths = []
+    row, col = start
+    visited = visited.union({start})
+    
+    # check if the top position is empty (walkable)
+    if is_top_bottom_empty(board, start, visited):
+        # walk the top position, if it is walkable
+        path_lengths = append_to_paths(path_lengths, 
+            recursive_finder(board, (row - 1, col), end, visited))
+    # check if the right position is empty (walkable)
+    if is_left_right_empty(board, start, visited, "right"):
+        # walk the right position, it it is walkable
+        path_lengths = append_to_paths(path_lengths, 
+            recursive_finder(board, (row, col + 1), end, visited))
+    # check if the bottom position is empty (walkable)
+    if is_top_bottom_empty(board, start, visited, "bottom"):
+        # walk the bottom position, it it is walkable
+        path_lengths = append_to_paths(path_lengths, 
+            recursive_finder(board, (row + 1, col), end, visited))
+    # check if the left position is empty (walkable)
+    if is_left_right_empty(board, start, visited):
+        # walk the left position, it it is walkable
+        path_lengths = append_to_paths(path_lengths, 
+            recursive_finder(board, (row, col - 1), end, visited))
+    
+    # return the minimum value from all possible paths, else return 0 (if there is no path)
+    return min(path_lengths) if len(path_lengths) else 0
+
+
+def append_to_paths(path_lengths:List, value:int) -> list:
+    """Append a value to a path lengths list
+
+    Args:
+        path_lengths (List): A list that contain already walked paths
+        value (int): The value that supposed to be appended to path lengths
+
+    Returns:
+        list: Appended list
+    """
+    # check if the value is 0, if not append it
+    if value: path_lengths.append(value)
+    return path_lengths
+
+
+def is_left_right_empty(
+    board:List[List[bool]], position:Tuple[int,int],
+    visited:Set, direction:str="left") -> bool:
+    """Check if either the left or right position is walkable
+
+    Args:
+        board (List[List[bool]]): A board
+        position (Tuple[int,int]): Current position
+        visited (Set): A set of visited positions
+        direction (str, optional): Which position to check, either left or right. Defaults to "left".
+
+    Returns:
+        bool: Returns True if it is walkable, False if not
+    """
+    row, col = position
+    col = col - 1 if direction == "left" else col + 1
+    return col != -1 and col < len(board[row]) and \
+        (row, col) not in visited and not board[row][col]
+
+
+def is_top_bottom_empty(
+    board:List[List[bool]], position:Tuple[int,int],
+    visited:Set, direction:str="top") -> bool:
+    """Check if either the top or bottom position is walkable
+
+    Args:
+        board (List[List[bool]]): A board
+        position (Tuple[int,int]): Current position
+        visited (Set): A set of visited positions
+        direction (str, optional): Which position to check, either top or bottom. Defaults to "top".
+
+    Returns:
+        bool: Returns True if it is walkable, False if not
+    """
+    row, col = position
+    row = row - 1 if direction == "top" else row + 1
+    return row != -1 and row < len(board) and \
+        (row, col) not in visited and not board[row][col]
 
 
 matrix = [[False, False, False, False],
           [True, True, False, True],
           [False, False, False, False],
           [False, False, False, False]]
+assert find_minimum_path(matrix, (0, 0), (0, 0)) == 0, "The value must be 0"
+assert find_minimum_path(matrix, (0, 1), (0, 0)) == 1, "The value must be 1"
+assert find_minimum_path(matrix, (3, 0), (0, 0)) == 7, "The value must be 7"
+assert find_minimum_path(matrix, (3, 0), (0, 3)) == 6, "The value must be 6"
+assert find_minimum_path(matrix, (3, 2), (0, 2)) == 3, "The value must be 3"
 
-finder = PathFinder(matrix)
-
-finder.find_minimum_path((1, 0), (0, 0))
-
-assert finder.find_minimum_path((0, 0), (0, 0)) == 0, "The value must be 0"
-assert finder.find_minimum_path((1, 0), (0, 0)) == 1, "The value must be 1"
-assert finder.find_minimum_path((3, 0), (0, 0)) == 7, "The value must be 7"
-assert finder.find_minimum_path((3, 0), (0, 3)) == 6, "The value must be 6"
 
 matrix = [[False, False, False, False],
           [True, True, True, False],
           [False, False, False, False],
           [False, False, False, False]]
-
-finder = PathFinder(matrix)
-
-assert finder.find_minimum_path((0, 0), (0, 0)) == 0
-assert finder.find_minimum_path((1, 0), (0, 0)) == 1
-assert finder.find_minimum_path((3, 0), (0, 0)) == 9
-assert finder.find_minimum_path((3, 0), (0, 3)) == 6
-assert finder.find_minimum_path((2, 0), (3, 3)) == 4
+assert find_minimum_path(matrix, (0, 0), (0, 0)) == 0
+assert find_minimum_path(matrix, (1, 0), (0, 0)) == 1
+assert find_minimum_path(matrix, (3, 0), (0, 0)) == 9
+assert find_minimum_path(matrix, (3, 0), (0, 3)) == 6
+assert find_minimum_path(matrix, (2, 0), (3, 3)) == 4
 
 
-check = {1, 2, 6}
-type(check)
-check.union({5})
-check 
+matrix = [[False, False, False, False],
+          [True, True, True, True],
+          [False, False, False, False],
+          [False, False, False, False]]
+find_minimum_path(matrix, (3, 0), (0, 0))
--- Problems/Day #24 Medium/solutions.py
+++ Problems/Day #24 Medium/solutions.py
@@ -1,68 +1,75 @@
+
 class Node:
-    def __init__(self) -> None:
-        self.path = ""
+    """A Node object"""
+    def __init__(self, value:str) -> None:
         self.locked = False
         self.left = None
         self.right = None
+        self.parent = None
+        self.value = value
+        self.is_any_children_locked = False
         
+    def is_locked(self) -> bool:
+        return self.locked
+    
+    def lock(self) -> None:
+        if self.is_locked() or self.is_any_children_locked \
+            or self.is_there_any_locked_ancestor():
+            return False
         
-def is_locked(node:Node) -> bool:
-    return node.locked
-
-def are_ancestors_unlocked(root:Node, node:Node) -> bool:
-    head = root
-    for direction in node.path:
-        if is_locked(head): return False
-        head = head.left if direction == "l" else head.right
-    return True
-
-def are_descendants_unlocked(node:Node) -> bool:
-    if is_locked(node):
+        self.locked = True
+        self.update_ancestors("locked")
+        return True
+    
+    def unlock(self) -> bool:
+        if not self.is_locked() or self.is_any_children_locked \
+            or self.is_there_any_locked_ancestor():
+            return False
+        
+        self.locked = False
+        self.update_ancestors("unlocked")
+        return True
+    
+    def update_ancestors(self, operation:str) -> None:
+        current_node = self.parent
+        while current_node:
+            current_node.is_any_children_locked = True if operation == "locked" else False
+            current_node = current_node.parent
+    
+    def is_there_any_locked_ancestor(self) -> bool:
+        current_node = self.parent
+        while current_node:
+            if current_node.is_locked(): return True
+            current_node = current_node.parent
+            
         return False
     
-    if node.left and node.right:
-        return are_descendants_unlocked(node.left) or are_descendants_unlocked(node.right)
-    elif node.left:
-        return are_descendants_unlocked(node.left)
-    elif node.right:
-        return are_descendants_unlocked(node.right)
-    else:
-        return True
-
-def lock(root:Node, node:Node) -> bool:
-    if are_ancestors_unlocked(root, node) and are_descendants_unlocked(node):
-        node.locked = True
-        return True
-    
-    return False
-
-
-def unlock(root:Node, node:Node) -> bool:
-    if are_ancestors_unlocked(root, node) and are_descendants_unlocked(node):
-        node.locked = False
-        return True
-    
-    return False
+    def add(self, value:str, direction:str="left") -> None:
+        child = Node(value)
+        if direction == "left":
+            self.left = child
+        else:
+            self.right = child
         
-    
-def add(parent:Node, direction:str="left") -> None:
-    child = Node()
-    if direction == "left":
-        parent.left = child
-        child.path = parent.path + "l"
-    else:
-        parent.right = child
-        child.path = parent.path + "r"
+        child.parent = self    
+        return child
         
         
-head = Node()
-add(head, "left")
-add(head, "right")
-add(head.left, "left")
-add(head.left, "right")
-add(head.right, "right")
-add(head.left.left, "left")
-add(head.left.left, "right")
+head = Node("root")
+l1 = head.add("l1", "left")
+r1 = head.add("l2", "right")
 
-lock(head, head.left)
-lock(head, head.left.left.right)
+l1l1 = l1.add("l1l1", "left")
+l1r1 = l1.add("l1r1", "right")
+
+r1r1 = r1.add("r1r1", "right")
+l1l2 = l1l1.add("l1l2", "left")
+
+
+l1l1.lock()  # True
+l1l2.lock()  # False
+head.lock()  # False
+head.is_any_children_locked  # True
+
+l1l1.unlock()  # True
+l1l2.lock()  # True
